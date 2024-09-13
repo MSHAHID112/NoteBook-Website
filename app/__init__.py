1d2e9a35bd8476c3e8418f37b1d6e7bb823f2aed
MSHAHID112 <101447290+MSHAHID112@users.noreply.github.com>
2024-09-14 02:48:48 +0500

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -2,13 +2,14 @@ from flask import Flask, redirect, url_for, request, jsonify, render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, current_user, login_required
 from markupsafe import Markup
+import os
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'secret-key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+mysqlconnector://root:12345@localhost/mydb'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.init_app(app)
     
